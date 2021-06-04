9e32ce2fe2573d45e17df59ab85ec24a830cd425
omarelmeligi <85227095+omarelmeligi@users.noreply.github.com>
2021-06-04 18:48:13 +0200

    Update to_str.h

--- to_str.h
+++ to_str.h
@@ -1,8 +1,13 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
-extern void To_string(float x,char* out);
-void To_string(float x,char* out)
+extern void To_str(double x, unsigned char* data);
+void To_str(double x, unsigned char* data)
 {
-sprintf(out,'%f',x);
+    char buff[50];
+    sprintf(buff, "%g", x);
+    for (int i = 0; buff[i] != 0; i++)
+    {
+        data[i] = buff[i];
+    }
 }
