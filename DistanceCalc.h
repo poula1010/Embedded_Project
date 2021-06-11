99e098c9d2ff141afe94c195573c355a9fb19479
michelle019 <85253330+michelle019@users.noreply.github.com>
2021-06-11 19:07:52 +0200

    Update DistanceCalc.h

--- DistanceCalc.h
+++ DistanceCalc.h
@@ -2,17 +2,17 @@
 #include <math.h>
 
 
-extern float Distance_Calculator(float long1, float lat1, float long2, float lat2);
-float Distance_Calculator(float long1, float lat1, float long2, float lat2)
+extern double Distance_Calculator(double long1, double lat1, double long2, double lat2);
+double Distance_Calculator(double long1, double lat1, double long2, double lat2)
 {
 	int Reart=6371e3;
-float M_PI  =3.142857;
-	float phi1 = lat1 * M_PI / 180;
-	float phi2 = lat2 * M_PI / 180;
-	float dphi = (lat2 - lat1) * M_PI / 180;
-	float dlambda = (long2 - long1) * M_PI / 180;
-	float a = pow(sin(dphi / 2), 2) + pow(cos(dphi / 2), 2) * pow(sin(dlambda / 2), 2);
-	float c = 2 * atan2(sqrt(a), sqrt(1 - a));
-	float d = Reart * c;
+double M_PI  =3.142857;
+	double phi1 = lat1 * M_PI / 180;
+	double phi2 = lat2 * M_PI / 180;
+	double dphi = (phi2 - phi1);
+	double dlambda = (long2 - long1) * M_PI / 180;
+	double a = pow(sin(dphi / 2), 2) + pow(cos(dphi / 2), 2) * pow(sin(dlambda / 2), 2);
+	double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+	double d = Reart * c;
 	return d;
 }
